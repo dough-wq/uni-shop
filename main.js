5f4f792e368f5b0b650160ffd258a5ed09638c2b
gggggihggg <1572569345@qq.com>
2022-09-10 14:29:42 +0800

    完成了首页功能的开发

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入$http对象
+import {
+  $http
+} from '@escook/request-miniprogram'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+// 请求开始之前做一些事情（请求拦截器）
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
+
+
 const app = new Vue({
-    ...App
+  ...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+  createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
   const app = createSSRApp(App)
