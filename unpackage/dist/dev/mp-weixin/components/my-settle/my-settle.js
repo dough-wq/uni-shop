22643128564ddd2f6457e0b289035e18b1bc59ef
gggggihggg <1572569345@qq.com>
2022-09-12 00:41:35 +0800

    完成登录支付

--- unpackage/dist/dev/mp-weixin/components/my-settle/my-settle.js
+++ unpackage/dist/dev/mp-weixin/components/my-settle/my-settle.js
@@ -114,7 +114,7 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@babel/runtime/regenerator */ 22));
 
 
 
@@ -131,34 +131,164 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 
 
 
-var _vuex = __webpack_require__(/*! vuex */ 14);function ownKeys(object, enumerableOnly) {var keys = Object.keys(object);if (Object.getOwnPropertySymbols) {var symbols = Object.getOwnPropertySymbols(object);if (enumerableOnly) symbols = symbols.filter(function (sym) {return Object.getOwnPropertyDescriptor(object, sym).enumerable;});keys.push.apply(keys, symbols);}return keys;}function _objectSpread(target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i] != null ? arguments[i] : {};if (i % 2) {ownKeys(Object(source), true).forEach(function (key) {_defineProperty(target, key, source[key]);});} else if (Object.getOwnPropertyDescriptors) {Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));} else {ownKeys(Object(source)).forEach(function (key) {Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));});}}return target;}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}var _default =
+var _vuex = __webpack_require__(/*! vuex */ 14);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function _slicedToArray(arr, i) {return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest();}function _nonIterableRest() {throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");}function _unsupportedIterableToArray(o, minLen) {if (!o) return;if (typeof o === "string") return _arrayLikeToArray(o, minLen);var n = Object.prototype.toString.call(o).slice(8, -1);if (n === "Object" && o.constructor) n = o.constructor.name;if (n === "Map" || n === "Set") return Array.from(o);if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);}function _arrayLikeToArray(arr, len) {if (len == null || len > arr.length) len = arr.length;for (var i = 0, arr2 = new Array(len); i < len; i++) {arr2[i] = arr[i];}return arr2;}function _iterableToArrayLimit(arr, i) {if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return;var _arr = [];var _n = true;var _d = false;var _e = undefined;try {for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {_arr.push(_s.value);if (i && _arr.length === i) break;}} catch (err) {_d = true;_e = err;} finally {try {if (!_n && _i["return"] != null) _i["return"]();} finally {if (_d) throw _e;}}return _arr;}function _arrayWithHoles(arr) {if (Array.isArray(arr)) return arr;}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};}function ownKeys(object, enumerableOnly) {var keys = Object.keys(object);if (Object.getOwnPropertySymbols) {var symbols = Object.getOwnPropertySymbols(object);if (enumerableOnly) symbols = symbols.filter(function (sym) {return Object.getOwnPropertyDescriptor(object, sym).enumerable;});keys.push.apply(keys, symbols);}return keys;}function _objectSpread(target) {for (var i = 1; i < arguments.length; i++) {var source = arguments[i] != null ? arguments[i] : {};if (i % 2) {ownKeys(Object(source), true).forEach(function (key) {_defineProperty(target, key, source[key]);});} else if (Object.getOwnPropertyDescriptors) {Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));} else {ownKeys(Object(source)).forEach(function (key) {Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key));});}}return target;}function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}var _default =
+
 
 
 
 
 {
   name: "my-settle",
-  computed: _objectSpread(_objectSpread({},
-  (0, _vuex.mapGetters)('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount'])), {}, {
+  computed: _objectSpread(_objectSpread(_objectSpread(_objectSpread({},
+  (0, _vuex.mapGetters)('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount'])),
+
+  (0, _vuex.mapGetters)('m_user', ['addstr'])),
+
+  (0, _vuex.mapState)('m_user', ['token'])), {}, {
     // 是否全选
     isFullCheck: function isFullCheck() {
       return this.total === this.checkedCount;
     } }),
 
   data: function data() {
-    return {};
-
+    return {
+      // 倒计时的秒数
+      seconds: 3,
+      // 定时器的 Id
+      timer: null };
 
   },
-  methods: _objectSpread(_objectSpread({},
+  methods: _objectSpread(_objectSpread(_objectSpread({},
 
-  (0, _vuex.mapMutations)('m_cart', ['updateAllGoodsState'])), {}, {
+  (0, _vuex.mapMutations)('m_cart', ['updateAllGoodsState'])),
+
+  (0, _vuex.mapMutations)('m_user', ['updateRedirectInfo'])), {}, {
+
     // label 的点击事件处理函数
     changeAllState: function changeAllState() {
       // 修改购物车中所有商品的选中状态
       // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
       this.updateAllGoodsState(!this.isFullCheck);
+    },
+    // 点击了结算按钮
+    settlement: function settlement() {
+      // 1. 先判断是否勾选了要结算的商品
+      if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！');
+
+      // 2. 再判断用户是否选择了收货地址
+      if (!this.addstr) return uni.$showMsg('请选择收货地址！');
+
+      // 3. 最后判断用户是否登录了，如果没有登录，则调用 delayNavigate() 进行倒计时的导航跳转
+      // if (!this.token) return uni.$showMsg('请先登录！')
+      if (!this.token) return this.delayNavigate();
+      // 4. 实现微信支付功能
+      this.payOrder();
+    },
+    // 微信支付
+    payOrder: function payOrder() {var _this = this;return _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee() {var orderInfo, _yield$uni$$http$post, res, orderNumber, _yield$uni$$http$post2, res2, payInfo, _yield$uni$requestPay, _yield$uni$requestPay2, err, succ, _yield$uni$$http$post3, res3;return _regenerator.default.wrap(function _callee$(_context) {while (1) {switch (_context.prev = _context.next) {case 0:
+                // 1. 创建订单
+                // 1.1 组织订单的信息对象
+                orderInfo = {
+                  // 开发期间，注释掉真实的订单价格，
+                  // order_price: this.checkedGoodsAmount,
+                  // 写死订单总价为 1 分钱
+                  order_price: 0.01,
+                  consignee_addr: _this.addstr,
+                  goods: _this.cart.filter(function (x) {return x.goods_state;}).map(function (x) {return {
+                      goods_id: x.goods_id,
+                      goods_number: x.goods_count,
+                      goods_price: x.goods_price };}) };
+
+
+                // 1.2 发起请求创建订单
+                _context.next = 3;return (
+
+                  uni.$http.post('/api/public/v1/my/orders/create', orderInfo));case 3:_yield$uni$$http$post = _context.sent;res = _yield$uni$$http$post.data;if (!(
+                res.meta.status !== 200)) {_context.next = 7;break;}return _context.abrupt("return", uni.$showMsg('创建订单失败！'));case 7:
+                // 1.3 得到服务器响应的“订单编号”
+                orderNumber = res.message.order_number;
+
+                // 2. 订单预支付
+                // 2.1 发起请求获取订单的支付信息
+                _context.next = 10;return (
+
+                  uni.$http.post('/api/public/v1/my/orders/req_unifiedorder', {
+                    order_number: orderNumber }));case 10:_yield$uni$$http$post2 = _context.sent;res2 = _yield$uni$$http$post2.data;if (!(
+
+
+                res2.meta.status !== 200)) {_context.next = 14;break;}return _context.abrupt("return", uni.$showError('预付订单生成失败！'));case 14:
+                // 2.3 得到订单支付相关的必要参数
+                payInfo = res2.message.pay;
+
+                // 3. 发起微信支付
+                // 3.1 调用 uni.requestPayment() 发起微信支付
+                _context.next = 17;return uni.requestPayment(payInfo);case 17:_yield$uni$requestPay = _context.sent;_yield$uni$requestPay2 = _slicedToArray(_yield$uni$requestPay, 2);err = _yield$uni$requestPay2[0];succ = _yield$uni$requestPay2[1];if (!
+
+                err) {_context.next = 23;break;}return _context.abrupt("return", uni.$showMsg('订单未支付！'));case 23:_context.next = 25;return (
+
+
+
+                  uni.$http.post('/api/public/v1/my/orders/chkOrder', {
+                    order_number: orderNumber }));case 25:_yield$uni$$http$post3 = _context.sent;res3 = _yield$uni$$http$post3.data;if (!(
+
+
+                res3.meta.status !== 200)) {_context.next = 29;break;}return _context.abrupt("return", uni.$showMsg('订单未支付！'));case 29:
+                // 3.5 检测到订单支付完成
+                uni.showToast({
+                  title: '支付完成！',
+                  icon: 'success' });case 30:case "end":return _context.stop();}}}, _callee);}))();
+
+    },
+    // 延迟导航到 my 页面
+    delayNavigate: function delayNavigate() {var _this2 = this;
+      // 把 data 中的秒数重置成 3 秒
+      this.seconds = 3;
+      // 1. 展示提示消息，此时 seconds 的值等于 3
+      this.showTips(this.seconds);
+
+      // 2. 创建定时器，每隔 1 秒执行一次
+      this.timer = setInterval(function () {
+        // 2. 判断秒数是否 <= 0
+        if (_this2.seconds <= 0) {
+          // 2.1 清除定时器
+          clearInterval(_this2.timer);
+
+          // 2.2 跳转到 my 页面
+          uni.switchTab({
+            url: '/pages/my/my',
+            // 页面跳转成功之后的回调函数
+            success: function success() {
+              // 调用 vuex 的 updateRedirectInfo 方法，把跳转信息存储到 Store 中
+              _this2.updateRedirectInfo({
+                // 跳转的方式
+                openType: 'switchTab',
+                // 从哪个页面跳转过去的
+                from: '/pages/cart/cart' });
+
+            } });
+
+
+          // 2.3 终止后续代码的运行（当秒数为 0 时，不再展示 toast 提示消息）
+          return;
+        }
+        _this2.showTips(_this2.seconds);
+      }, 1000);
+    },
+    // 展示倒计时的提示消息
+    showTips: function showTips(n) {
+      // 调用 uni.showToast() 方法，展示提示消息
+      uni.showToast({
+        // 不展示任何图标
+        icon: 'none',
+        // 提示的消息
+        title: '请登录后再结算！' + n + ' 秒后自动跳转到登录页',
+        // 为页面添加透明遮罩，防止点击穿透
+        mask: true,
+        // 1.5 秒后自动消失
+        duration: 1500 });
+
     } }) };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 
